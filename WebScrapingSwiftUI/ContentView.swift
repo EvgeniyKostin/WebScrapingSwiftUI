6e4aef0816bfd5726c412f216822e8f506d95f27
Evgeniy Kostin <kostinen@icloud.com>
2024-05-04 20:29:41 +0300

    Test SwiftSoup

--- WebScrapingSwiftUI/ContentView.swift
+++ WebScrapingSwiftUI/ContentView.swift
@@ -6,16 +6,21 @@
 //
 
 import SwiftUI
+import SwiftSoup
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Button("Run SwiftSoup", action: {
+            let html = "<html><head><title>First parse</title></head>" +
+            "<body><p>Parsed HTML into a doc.</p></body></html>"
+            
+            do{
+                let document:Document = try SwiftSoup.parse(html)
+                return try print(document.text())
+            }catch{
+                print("Error")
+            }
+        })
     }
 }
 
