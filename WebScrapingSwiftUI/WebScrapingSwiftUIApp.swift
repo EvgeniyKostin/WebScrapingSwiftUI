71b980fc69b829dc44a4b53dd1c6ac05b3bfd681
Evgeniy Kostin <kostinen@icloud.com>
2024-05-04 19:43:13 +0300

    Initial Commit

---
+++ WebScrapingSwiftUI/WebScrapingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebScrapingSwiftUIApp.swift
+//  WebScrapingSwiftUI
+//
+//  Created by Evgeniy Kostin on 04.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WebScrapingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
