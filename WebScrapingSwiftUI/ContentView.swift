d5945e0d66538574313636b91f751e97861c2dde
Evgeniy Kostin <kostinen@icloud.com>
2024-05-05 11:18:22 +0300

    Get a Link

--- WebScrapingSwiftUI/ContentView.swift
+++ WebScrapingSwiftUI/ContentView.swift
@@ -12,11 +12,14 @@ struct ContentView: View {
     var body: some View {
         Button("Run SwiftSoup", action: {
             let html = "<html><head><title>First parse</title></head>" +
-            "<body><p>Parsed HTML into a doc.</p></body></html>"
+            "<body><p>Parsed HTML into a doc.</p> <p>An <a href='http://example.com/'><b>example</b> link.</p></body></html>"
             
             do{
                 let document:Document = try SwiftSoup.parse(html)
-                return try print(document.text())
+                let link: Element = try document.select("a").first()!
+                let getLink = try link.attr("href")
+                
+                return print(getLink)
             }catch{
                 print("Error")
             }
